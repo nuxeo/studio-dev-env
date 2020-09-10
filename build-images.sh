d9f7063a65000a17859fff1cd9bf60dd77aba26c
Arnaud Kervern <akervern@nuxeo.com>
2020-09-10 15:07:11 +0200

    Rewrite Image inheritance

--- build-images.sh
+++ build-images.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -ex
 
+# Build dev base image
+docker build -t akervern/dev-base dev-base
+
 # build base image
-docker build -t akervern/code-server-base code-server-base
+# docker build -t akervern/code-server-base code-server-base
+docker build -t akervern/theia-builder theia-builder
+docker build -t akervern/theia-base theia-base
 
 # build code-server + maven
-docker build -t akervern/code-server-java code-server-java
+# docker build -t akervern/code-server-java code-server-java
 
 # build project image
-docker build -t akervern/project-code-server project-code-server
+# docker build -t akervern/project-code-server project-code-server
+docker build -t akervern/project-theia project-theia
--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
-#docker run --rm -it -p 127.0.0.1:8080:8080 -v "/Users/arnaud/Nuxeo/sources/tmp/toto:/home/coder/project/project" -e "PROJECT=akervern-SANDBOX" -e "USERNAME=akervern" -e "TOKEN=${TOKEN}" -e "EXTENSIONS=redhat.java,vscjava.vscode-maven" akervern/studio-code-server:latest
+#!/bin/bash
 
-# Start CodeServer-Project
-#docker run --rm -it -p 127.0.0.1:8080:8080 -v "/Users/arnaud/Nuxeo/tmp/toto-vscode:/home/coder/project/project" -v "/Users/arnaud/.m2/repository:/home/coder/.m2/repository" -e "PROJECT=akervern-SANDBOX" -e "USERNAME=akervern" -e "TOKEN=${TOKEN}" -e "EXTENSIONS=''" akervern/project-code-server:latest
+IDE=${1:-code-server}
 
-# Start Theia
-docker run --rm -it -p 127.0.0.1:8080:8080 -v "/Users/arnaud/Nuxeo/tmp/toto-vscode:/home/project/project" -v "/Users/arnaud/.m2/repository:/home/theia/.m2/repository" -e "PROJECT=akervern-SANDBOX" -e "USERNAME=akervern" -e "TOKEN=${TOKEN}" akervern/project-theia:latest
+if [ "code-server" == ${1} ]; then
+  # Start CodeServer-Project
+  exec docker run --rm -it -p 127.0.0.1:8080:8080 -v "/Users/arnaud/Nuxeo/tmp/toto-vscode:/home/coder/project/project" -v "/Users/arnaud/.m2/repository:/home/coder/.m2/repository" -e "PROJECT=akervern-SANDBOX" -e "USERNAME=akervern" -e "TOKEN=${TOKEN}" -e "EXTENSIONS=''" akervern/project-code-server:latest
+fi
+
+if [ "theia" == ${1} ]; then
+  # Start Theia
+  docker run --rm -it -p 127.0.0.1:8080:8080 -v "/Users/arnaud/Nuxeo/tmp/toto-vscode:/home/project/project" -v "/Users/arnaud/.m2/repository:/home/theia/.m2/repository" -e "PROJECT=akervern-SANDBOX" -e "USERNAME=akervern" -e "TOKEN=${TOKEN}" akervern/project-theia:latest
+fi
